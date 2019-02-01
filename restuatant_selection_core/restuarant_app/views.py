53f9a25074f27a86c9b1552446309ee0d893c7df
Kyande <kyande20@gmail.com>
2019-02-01 12:19:54 +0300

    django wip

--- restuatant_selection_core/restuarant_app/views.py
+++ restuatant_selection_core/restuarant_app/views.py
@@ -5,8 +5,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'restuarant_app/index.html', {})
+    return render(request, 'restuarant_app/index.html', {'text': "hello world"})
 
 
 def suggestions(request):
     return render(request, 'restuarant_app/suggestions.html', {})
+
+
+def about_us(request):
+    return render(request, 'restuarant_app/about.html', {})
